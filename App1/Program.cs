46852b05ae6c9fb696b71d9d514ba626000d32f3
Graham Hoggan <grahambentleyhoggan@gmail.com>
2025-05-05 18:58:32 -0700

    made it work on bens PC

--- App1/Program.cs
+++ App1/Program.cs
@@ -2,37 +2,65 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Diagnostics;
+using System.IO;
 
 public class Program
 {
+    static string logsPath = System.AppDomain.CurrentDomain.BaseDirectory+"/DebugLogs.txt";
     public static void Main(string[] args)
     {
+        Log("\n***************************\nSession Started: "+SystemClock.Instance.Now);
         IWebDriver driver = new ChromeDriver();
         try{
-            driver.Navigate().GoToUrl("https://testpages.herokuapp.com/styled/basic-html-form-test.html");
-            Console.WriteLine("Opened Succesfully");
+            driver.Navigate().GoToUrl("https://192.168.1.43/ui/apps/");
+            Log("Opened Website Successfully");
                         Thread.Sleep(1000);
 
-            var buttons = driver.FindElements(By.CssSelector("input[name='submitbutton'][value='submit']"));
-            if(buttons.Any())
+            var updateAllButtons = driver.FindElements(By.CssSelector("ixtest='update-all'][class='update-all-button mdc-button mat-mdc-button mat-unthemed mat-mdc-button-base ng-star-inserted']"));
+            if(updateAllButtons.Any())
             {
-            Console.WriteLine("submit button found");
-            buttons.First().Click();
-            Console.WriteLine("Button clicked");
+                
+            Log("update all button found");
+            updateAllButtons.First().Click();
+            Log("update all Button clicked");
             Thread.Sleep(1000);
             }
             else
             {
-                Console.WriteLine("No Such Element");
+                Log("No update all Element");
+            }
+            var confirmButtons = driver.FindElements(By.CssSelector("ixtest='upgrade'][class='mdc-button mat-mdc-button mat-primary mat-mdc-button-base ng-star-inserted']"));
+            if(confirmButtons.Any())
+            {
+            Log("Confirm button found");
+            confirmButtons.First().Click();
+            Log("Confirm button clicked");
+            Thread.Sleep(1000);
+            }
+            else
+            {
+                Log("No Confirm Element");
             }
         }
         catch(Exception e)
         {
-            Console.WriteLine($"You messed up: {e.Message}");
+            Log($"You messed up: {e.Message}");
         }
         finally
         {
             driver.Quit();
         }
+    }
+    private static void Log(string msg)
+    {
+        string dat = "";
+        if(File.Exists(logsPath))
+        {
+dat = File.ReadAllText(logsPath);
+        }
+        
+        dat+="\n"+msg;
+        File.WriteAllText(logsPath,dat);
     }
 }
--- App1/obj/Debug/net9.0/App1.AssemblyInfo.cs
+++ App1/obj/Debug/net9.0/App1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e823f31ccfb751109b9fe506c529c4b6889314bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("App1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
